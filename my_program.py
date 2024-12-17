247955113c096f3d1a65615d80f52ddeb4910255
GIL32610 <gil.00000032610@unicap.br>
2024-12-17 12:10:36 -0300

    printing the first 20 even numbers

--- my_program.py
+++ my_program.py
@@ -1,5 +1,5 @@
 if __name__ == 'main':
-    #printing 10 first even numbers
-    for number in range(11):
+    #printing 20 first even numbers
+    for number in range(21):
         if number%2 == 0:
             print("Number is: ", number) 
